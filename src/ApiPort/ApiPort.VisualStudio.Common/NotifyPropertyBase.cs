bcc2d33b6f2dbebe1c9f5139ba9cbe58456cf93a
Taylor Southwick <tasou@microsoft.com>
2019-07-10 14:52:00 -0700

    Add thread check

--- src/ApiPort/ApiPort.VisualStudio.Common/NotifyPropertyBase.cs
+++ src/ApiPort/ApiPort.VisualStudio.Common/NotifyPropertyBase.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Microsoft.VisualStudio.Shell;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -10,6 +11,8 @@ namespace ApiPortVS
 {
     public class NotifyPropertyBase : INotifyPropertyChanged
     {
+        private static bool _switchThread = IsInVs();
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public NotifyPropertyBase()
@@ -34,9 +37,24 @@ namespace ApiPortVS
                 return;
             }
 
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            if (_switchThread)
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            }
 
             propertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private static bool IsInVs()
+        {
+            try
+            {
+                return ThreadHelper.JoinableTaskFactory != null;
+            }
+            catch (NullReferenceException)
+            {
+                return false;
+            }
         }
     }
 }
